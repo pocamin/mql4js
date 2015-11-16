9d916cfffce652e56d8f68c4b189aff62f86de07
bleroux <benja.leroux@gmail.com>
2015-11-16 13:21:33 +0100

    update gitIgnore to prevent commiting wrong files

--- gulpfile.js
+++ gulpfile.js
@@ -81,7 +81,7 @@ gulp.task('bower', function () {
 gulp.task('html', ['bower'], function () {
   return gulp.src('app/*.html')
     .pipe(template({samples: fs.readdirSync("app/mql4/samples")}))
-    .pipe(useref())
+    .pipe(useref())ls
     /*.pipe(gulpif('js/*.js', uglify()))*/
     .pipe(gulpif('*.css', minifyCss()))
     .pipe(gulp.dest('dist'))
